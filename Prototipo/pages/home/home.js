038e71c3b67da0aa79fae649fc9b7312f25eaacb
Leonardo Cassauara <leomaia.rj@hotmail.com>
2023-12-15 20:54:34 -0300

    update homepage and icon

--- Prototipo/pages/home/home.js
+++ Prototipo/pages/home/home.js
@@ -1,15 +1,3 @@
-function logout() {
-    showLoading()
-    firebase.auth().signOut().then(() => {
-        hideLoading()
-        window.location.href = "../../index.html"
-    }).catch(error => {
-        hideLoading()
-        alert("Erro, tente novamente.")
-    })
-}
-
-
 const columns = {
     todo: () => document.getElementById('todo'),
     doing: () => document.getElementById('doing'),
@@ -26,6 +14,7 @@ const columnsField = {
 }
 
 
+/*
 const fakeKanban = [
     [
         {title: "Estudar"},
@@ -41,10 +30,22 @@ const fakeKanban = [
         {title: "Período"},
     ],
 ]
+*/
 
-findCards()
 
-// Simular o Backend (fake)
+const fakeKanban2 = {
+    todo: ["Estudar", "Relaxar"],
+    doing: ["Trainee"],
+    review: ["Abordagem", "Backend"],
+    done: ["Período"]
+}
+
+
+findCards2()
+
+
+// Modelo Fake
+/*
 function findCards() {
     setTimeout(() => {
         addCardsToScreen(fakeKanban);
@@ -66,7 +67,7 @@ function addCardsToScreen(cards) {
             const li = document.createElement('li');
             li.className = "card";
             li.innerText = card.title
-            
+
             if (i == 0) todoField.appendChild(li);
             else if (i == 1) doingField.appendChild(li);
             else if (i == 2) reviewField.appendChild(li);
@@ -74,5 +75,74 @@ function addCardsToScreen(cards) {
         })
         
         i++;
+    })
+}
+*/
+
+// Modelo da Firebase
+function findCards2() {
+    showLoading();
+    firebase.firestore()
+        .collection('colunas')
+        .get()
+        .then(snapshot => {
+            hideLoading()
+            let cards;
+            snapshot.docs.forEach(doc => {
+                cards = doc.data()
+            })
+            addCardsToScreen2(cards)
+        }).catch(error => {
+            hideLoading()
+            console.log(error)
+            alert("Erro, tente novamente.")
+        })
+}
+
+
+function addCardsToScreen2(cards) {
+    const todoField = columnsField.ulTodo()
+    const doingField = columnsField.ulDoing()
+    const reviewField = columnsField.ulReview()
+    const doneField = columnsField.ulDone()
+
+    cards.todo.forEach(card => {
+        const li = document.createElement('li');
+        li.className = "card";
+        li.innerText = card
+        todoField.appendChild(li)
+    })
+
+    cards.doing.forEach(card => {
+        const li = document.createElement('li');
+        li.className = "card";
+        li.innerText = card
+        doingField.appendChild(li)
+    })
+
+    cards.review.forEach(card => {
+        const li = document.createElement('li');
+        li.className = "card";
+        li.innerText = card
+        reviewField.appendChild(li)
+    })
+
+    cards.done.forEach(card => {
+        const li = document.createElement('li');
+        li.className = "card";
+        li.innerText = card
+        doneField.appendChild(li)
+    })
+}
+
+
+function logout() {
+    showLoading()
+    firebase.auth().signOut().then(() => {
+        hideLoading()
+        window.location.href = "../../index.html"
+    }).catch(error => {
+        hideLoading()
+        alert("Erro, tente novamente.")
     })
 }
