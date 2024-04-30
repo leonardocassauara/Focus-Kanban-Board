0874ef0b748a6cd5b8ca32eec840ab41fdf2eefc
Leonardo Cassauara <124696947+leonardocassauara@users.noreply.github.com>
2024-04-30 14:58:18 -0300

    chore: configurando variável firebase

--- Prototipo/firabaseInit.js
+++ Prototipo/firabaseInit.js
@@ -1,11 +1,11 @@
 (function() {
     const firebaseConfig = {
-        apiKey: "AIzaSyAqyBmCL9hD7qc0Uz73_tcQldTN1JDp1nY",
-        authDomain: "focus-kanban-board.firebaseapp.com",
-        projectId: "focus-kanban-board",
-        storageBucket: "focus-kanban-board.appspot.com",
-        messagingSenderId: "174381111412",
-        appId: "1:174381111412:web:e40c26ee6a79a6bf3b1921"
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: ""
       };
 
     firebase.initializeApp(firebaseConfig)
